3a1e32e43d6f6e4abfa998e684b418b4f87be450
Tyler Vanderhoef <tyler.j.vanderhoef@gmail.com>
2017-01-20 20:09:48 -0800

    Added nuget deploy script.

---
+++ nuget-deploy.ps1
@@ -0,0 +1,21 @@
+# Determine the actual name of the package.
+$branch = $env:APPVEYOR_REPO_BRANCH
+$pr = $env:APPVEYOR_PULL_REQUEST_NUMBER
+
+if($pr -ne "") {
+   # Do nothing as this is a PR
+   return
+}
+
+if($branch -eq "cert") {
+    # Set beta version, rather than the production version name.
+    $env:nuget_name += "-beta"
+}
+elseif($branch -ne "master") {
+   # Must be a dev build.
+   $env:nuget_name += "-dev"
+}
+
+# Build nuget package. 
+Write-Host "Packaging nuget with name: $env:nuget_name"
+Write-Host "id=$env:nuget_name"
